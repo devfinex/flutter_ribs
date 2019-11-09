2f3deb4608e631a00f051f2330176f495a5e8cc0
devfinex <devfinex@gmail.com>
2019-11-10 03:29:33 +0900

    update gitignore

--- example/lib/app_start/app_component.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ribs/ribs.dart';
-
-import '../root/root_builder.dart';
-
-/// The top level component with no dependencies
-class AppComponent extends Component<EmptyDependency> implements RootDependency {
-  AppComponent() : super(EmptyComponent());
-}
